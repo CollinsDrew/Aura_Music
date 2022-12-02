7c18994b9af64ee1a5dad40387bee06b789076a8
CollinsDrew <collinsdrew1990@gmail.com>
2022-12-02 18:40:28 +0000

    logo icon updated

--- src/Components/LeftMenu.js
+++ src/Components/LeftMenu.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "../Styles/LeftMenu.css";
 
-import { FaSpotify, FaEllipsisH } from "react-icons/fa";
+import { FaMicrophoneAlt, FaEllipsisH } from "react-icons/fa";
 
 function LeftMenu() {
   return (
     <div className="leftMenu">
       <div className="logoContainer">
         <i>
-          <FaSpotify />
+          <FaMicrophoneAlt />
         </i>
         <h2>Aura</h2>
         <i>

32114259b797fd2c842a0cfa42b2bb1d8a3e550b
CollinsDrew <collinsdrew1990@gmail.com>
2022-12-03 12:59:10 +0000

    basic music player styles modified

--- src/Components/TrackList.js
+++ src/Components/TrackList.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BsVolumeUpFill, BsMusicNoteList } from "react-icons/bs";
 import { FaDesktop } from "react-icons/fa";
+import Track from "../Img/Track.webp";
 
 function TrackList() {
   return (
     <div className="trackList">
       <div className="top">
-        <img src="" alt="" />
-        <p>
-          Sample Name<span>Artist</span>
+        <img src={Track} alt="Track" />
+        <p className="trackName">
+          Sample Name<span className="trackSpan">Artist</span>
         </p>
       </div>
       <div className="bottom">

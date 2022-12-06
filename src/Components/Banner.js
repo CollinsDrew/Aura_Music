3aa87500affaaf08ee987f67f996d2d5e928735e
CollinsDrew <collinsdrew1990@gmail.com>
2022-12-06 16:47:45 +0000

    style updates to banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Aura_Banner from "../Img/AuraBanner.png";
 import Check from "../Img/check.png";
+import { FaEllipsisH, FaHeadphones, FaCheck } from "react-icons/fa";
 
 function Banner() {
   return (
     <div className="banner">
       <img src={Aura_Banner} alt="" className="bannerImage" />
+
+      <div className="content">
+        <div className="breadCrumb">
+          <p>Home</p>
+
+          <i>
+            <FaEllipsisH />
+          </i>
+        </div>
+        <div className="bottomLayer"></div>
+      </div>
     </div>
   );
 }

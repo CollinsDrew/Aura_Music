4c3e654ca565718a62fa313962a0dc3193630344
CollinsDrew <collinsdrew1990@gmail.com>
2022-12-07 19:02:02 +0000

    key notes added on audiolistcomponent

--- src/Components/AudioList.js
+++ src/Components/AudioList.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Styles/AudioList.css";
 
 import { FaRegClock, FaHeart, FaRegHeart } from "react-icons/fa";
 
 function AudioList() {
+  // const [song, setSong] = useState (Songs[0].song);
+  // const [img, setImage] = useState (Songs[0].imgSrc);
+
   return (
     <div className="audioList">
       <h2 className="title">
+        {/* In span tag `${songs.length} songs` */}
         The List <span>x songs</span>
       </h2>
 
       <div className="songsContainer">
+        {/* {
+            songs && songs.map((song, index) => (
+
+            ))
+          } */}
+
+        {/* the songs div needs to be moved in to the map */}
+        {/* key ={song.id} */}
         <div className="songs">
+          {/* `#${index + 1}` */}
           <div className="count">#01</div>
           <div className="song">
             <div className="imgBox">
@@ -19,8 +32,10 @@ function AudioList() {
 
               <div className="section">
                 <p className="songName">
+                  {/* {song?.songName} */}
                   Sample <span className="spanArtist">Artist Name</span>
                 </p>
+                {/* {song?.artist} */}
 
                 <div className="hits">
                   <p className="duration">
@@ -31,6 +46,10 @@ function AudioList() {
                   </p>
 
                   <div className="favourite">
+                    {/* {song?.favourite ?
+                    FaHeart
+                  : FaRegHeart} */}
+
                     <i>
                       <FaHeart />
                     </i>

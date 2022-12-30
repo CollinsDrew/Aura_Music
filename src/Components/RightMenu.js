ed31808147537660786fe2402cc1cabd2305f1e9
CollinsDrew <collinsdrew1990@gmail.com>
2022-12-30 12:04:19 +0000

    right menu styling

--- src/Components/RightMenu.js
+++ src/Components/RightMenu.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "../Styles/RightMenu.css";
+import { BsFillFilePersonFill } from "react-icons/bs";
+import { FaUpload } from "react-icons/fa";
 
 function RightMenu() {
-  return <div className="rightMenu"></div>;
+  return (
+    <div className="rightMenu">
+      <div className="profile">
+        <i>
+          <BsFillFilePersonFill />
+        </i>
+      </div>
+      <div className="upload">
+        <i>
+          <FaUpload />
+          <p>Submit Tracks</p>
+        </i>
+      </div>
+    </div>
+  );
 }
 
 export { RightMenu };
